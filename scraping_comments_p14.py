519608efabdef93d44a7686d64b6045fcda6aa81
AlonsoPuente <alonso.puenterios@gmail.com>
2020-08-29 18:46:52 -0500

    Update scraping_comments_p14.py

--- scraping_comments_p14.py
+++ scraping_comments_p14.py
@@ -21,6 +21,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from random import randint
 import time
 import re
+import random
+from datetime import datetime
+random.seed(datetime.now())
 
 print('Comenzando a correr el script')
 
@@ -107,7 +110,7 @@ def getPageText(url):
 
 print("Scraping...")
 
-count_proj=22143   
+count_proj=22142
 for id_proj, url_proj in zip(data_final["id"][22143:23845], urls_list[22143:23845]):
     comments_txt = getPageText(url_proj)    
     df_comments = {"ids":[id_proj], "comments":[comments_txt]}
