e55c49699f1e36f416a278a440fcc12339f12f32
AlonsoPuente <alonso.puenterios@gmail.com>
2020-09-03 02:10:08 -0500

    Update scraping_comments_p05.py

--- scraping_comments_p05.py
+++ scraping_comments_p05.py
@@ -113,7 +113,7 @@ for id_proj, url_proj in zip(data_final["id"][6815:8518], urls_list[6815:8518]):
     comments_txt = getPageText(url_proj)    
     df_comments = {"ids":[id_proj], "comments":[comments_txt]}
     data_comments = pd.DataFrame(df_comments)
-    data_comments.to_csv (r'data_comentarios_p5.csv', mode = 'a', sep = ',', index = False, header=False)
+    data_comments.to_csv (r'data_comentarios_p05.csv', mode = 'a', sep = ',', index = False, header=False)
     count_proj += 1
     print(count_proj)
         
