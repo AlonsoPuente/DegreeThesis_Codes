862086271f8dfbe82a2f7a2da4b6cfd81c6781a5
AlonsoPuente <alonso.puenterios@gmail.com>
2020-09-21 02:04:05 -0500

    Update scraping_pledge_amounts_p01.py

--- scraping_pledge_amounts_p01.py
+++ scraping_pledge_amounts_p01.py
@@ -59,7 +59,7 @@ options.add_argument("--window-size=1920,1200")
 options.add_argument('--no-sandbox')
 
 def getPageText(url):
-    driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+    driver = webdriver.Chrome(options=options)
     driver.get(url)
     
     try:
